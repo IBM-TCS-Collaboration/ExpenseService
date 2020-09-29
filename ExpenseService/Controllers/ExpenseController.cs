b9d401ca0c211328bb922a7bf29cdba7fc36ed12
taskeen16 <65477432+taskeen16@users.noreply.github.com>
2020-09-29 13:56:14 +0530

    Update ExpenseController.cs

--- ExpenseService/Controllers/ExpenseController.cs
+++ ExpenseService/Controllers/ExpenseController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,8 +23,8 @@ namespace ExpenseService.Controllers
             db = new DatabaseContext();
 
         }
-      
-
+        
+        
         // GET api/<ExpenseController>/5
         [ActionName("getExpense")]
         [HttpGet("{UserId}")]
